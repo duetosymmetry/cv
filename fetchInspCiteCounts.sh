7aa40c31383b20eacc648a0aa2347ea105048eb2
Leo C. Stein <leo.stein@gmail.com>
2021-02-05 23:04:30 -0600

    Use old.inspirehep for fetching citation counts...

--- fetchInspCiteCounts.sh
+++ fetchInspCiteCounts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl "http://inspirehep.net/search?ln=en&ln=en&p=exactauthor%3AL.C.Stein.2&of=hlxu&action_search=Search&sf=earliestdate&so=d&rm=&rg=250&sc=0" | gsed -n -e "s/;%%<br \/>  %/: /g" -e "s/.*CITATION = \(.*\) counted.*/\1/gp" > citeCounts.txt
+curl "http://old.inspirehep.net/search?ln=en&ln=en&p=exactauthor%3AL.C.Stein.2&of=hlxu&action_search=Search&sf=earliestdate&so=d&rm=&rg=250&sc=0" | gsed -n -e "s/;%%<br \/>  %/: /g" -e "s/.*CITATION = \(.*\) counted.*/\1/gp" > citeCounts.txt
